fb96b729ba72eee978d8695033e2300c989fea75
Carlos Ringer <carlosringer@gmail.com>
2016-04-01 16:08:15 -0400

    commiting changes to user controller and router.ex

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,9 +1,8 @@
 defmodule Rumbl.Repo do
-  #use Ecto.Repo, otp_app: :rumbl
+  use Ecto.Repo, otp_app: :rumbl
 
 @moduledoc """
 In memory repository"
-"""
   def all(Rumbl.User) do
     [%Rumbl.User{id: "1", name: "Jose", username: "josevalim", password: "elixir"},
     %Rumbl.User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
@@ -22,5 +21,7 @@ In memory repository"
       Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
     end
   end
+"""
+
 
 end
--- web/router.ex
+++ web/router.ex
@@ -17,7 +17,10 @@ defmodule Rumbl.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
   end
+
+
 
   # Other scopes may use custom stacks.
   # scope "/api", Rumbl do
